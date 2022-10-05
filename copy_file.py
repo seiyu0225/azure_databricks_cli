ba2a551c2b80c7d11f382f634ff8120b500bcff4
okai <seiyu0225@gmail.com>
2022-10-05 22:43:51 +0900

    rename

--- copy_file.py
+++ copy_file.py
@@ -4,7 +4,7 @@ from re import sub
 import subprocess
 from tqdm import tqdm
 
-source_directory = "dbfs:/FileStore/okai/"
+source_directory = "dbfs:/FileStore/xxxxxxx"
 target_directory = "./data/"
 
 file_list = subprocess.run([f"dbfs ls {source_directory}"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
